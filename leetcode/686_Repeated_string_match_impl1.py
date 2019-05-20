8c20aacc6a77ef0a8cb8f6721af1b3425168f15d
Aastha Agrawal <aasthaagrawal94@gmail.com>
2019-05-20 15:16:19 +0530

    Repeated string match

---
+++ leetcode/686_Repeated_string_match_impl1.py
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/repeated-string-match/
+#Time Complexity: O(N*(M+N)) 
+
+class Solution:
+    def repeatedStringMatch(self, A: str, B: str) -> int:
+        q=(len(B)-1)//len(A)+1
+        for i in range(2):
+            if B in A * (q+i):
+                return q+i
+            
+        return -1

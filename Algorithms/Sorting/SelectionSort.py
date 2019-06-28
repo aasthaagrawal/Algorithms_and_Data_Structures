a240a50c2d9478514e87652522de94a25cf5c3fb
Aastha Agrawal <aasthaagrawal94@gmail.com>
2019-06-28 22:44:58 +0530

    Modified Selection Sort

--- Algorithms/Sorting/SelectionSort.py
+++ Algorithms/Sorting/SelectionSort.py
@@ -7,8 +7,7 @@ def SelectionSort(arr):
 		for j in range(i+1,l):
 			if arr[minI]>arr[j]:
 				minI=j
-		if minI is not i:
-			arr[i],arr[minI]=arr[minI],arr[i]
+		arr[i],arr[minI]=arr[minI],arr[i]
 
 arr=[10,80,30,90,40,50,70]
 SelectionSort(arr)
